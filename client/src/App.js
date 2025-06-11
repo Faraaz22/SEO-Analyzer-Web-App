6a8a24e324d37aae36edc048bda0a62a2ab14761
Faraaz Mahmood <faraazmahmood22@gmail.com>
2025-06-12 00:04:38 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -27,50 +27,53 @@ function App() {
       });
   }
 
-  // Insert keyword into updated text
   function insertKeyword(word) {
-    if (updatedText.includes(word)) return;
-    const words = updatedText.trim().split(' ');
-    words.splice(2, 0, word);
-    setUpdatedText(words.join(' '));
-  }
+  const wordList = updatedText.trim().split(/\s+/);
+  if (wordList.includes(word)) return;
 
+  setUpdatedText(prev => prev.trim() + ' ' + word);
+}
 
   return (
     <div className="App">
-      {/* LEFT SIDE */}
-      <div className="left">
-        <h3>Input Text:</h3>
-        <textarea
-          rows="6"
-          value={text}
-          onChange={handleChange}
-          placeholder="Enter your text here"
-        />
-        <button onClick={handleSubmit}>Analyze</button>
-
-        {Object.keys(keywords).length > 0 && (
-          <div className="results">
-            <h3>Keywords:</h3>
-            <ul>
-              {Object.entries(keywords).map(([kw, count], i) => (
-                <li key={i}>
-                  {kw} ({count})
-                  <button class="libtn" onClick={() => insertKeyword(kw)}>Insert</button>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+      <div className="navbar">
+        <h1>SEO Analyzer Web Page</h1>
       </div>
+      {/* LEFT SIDE */}
+      <div className='page'>
+        <div className="left">
+          <h3>Input Text:</h3>
+          <textarea
+            rows="6"
+            value={text}
+            onChange={handleChange}
+            placeholder="Enter your text here"
+          />
+          <button onClick={handleSubmit}>Analyze</button>
 
-      {/* RIGHT SIDE */}
-      <div className="right">
-        <h3>Updated Text:</h3>
-        <p className="text-preview">{updatedText}</p>
+          {Object.keys(keywords).length > 0 && (
+            <div className="results">
+              <h3>Keywords:</h3>
+              <ul>
+                {Object.entries(keywords).map(([kw, count], i) => (
+                  <li key={i}>
+                    {kw} :{count}
+                    <button class="libtn" onClick={() => insertKeyword(kw)}>Insert</button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="right">
+          <h3>Updated Text:</h3>
+          <p className="text-preview">{updatedText}</p>
           <button onClick={() => navigator.clipboard.writeText(updatedText)}>
             Copy Text
           </button>
+        </div>
       </div>
     </div>
   );
