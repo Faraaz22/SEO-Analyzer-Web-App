c4f0d9a09ca8fe21fb32b8f6b36534e916c5292a
Faraaz Mahmood <faraazmahmood22@gmail.com>
2025-06-12 16:19:51 +0530

    updated

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,11 @@ import './App.css';
 
 function App() {
   // States
+  // store input text
   const [text, setText] = useState('');
+  // store bresponse text
   const [updatedText, setUpdatedText] = useState('');
+  // object that maps keyword and frequency 
   const [keywords, setKeywords] = useState({});
 
   // Handle textarea input
@@ -15,49 +18,62 @@ function App() {
 
   // Handle analyze button click
   function handleSubmit() {
+    // Sends a POST request to /api/analyze with the input text
     axios.post('http://localhost:3001/api/analyze', { text })
       .then((response) => {
+        // store response data
         const data = response.data;
+        // sets updatedText and keywords from response.
         setUpdatedText(data.updatedText);
-        setKeywords(data.keywords || {}); //  Convert object to array
+        //  sets the keywords object
+        setKeywords(data.keywords || {});
       })
       .catch((error) => {
+        // EDGE CASE: If analysis fails/not reachable/no internet
         alert("Analysis Failed");
         console.log(error);
       });
   }
 
   function insertKeyword(word) {
-  const wordList = updatedText.trim().split(/\s+/);
-  if (wordList.includes(word)) return;
-
-  setUpdatedText(prev => prev.trim() + ' ' + word);
-}
+    // trims the updatedText and splits it into words
+    const wordList = updatedText.trim().split(/\s+/);
+    // prevents inserting duplicates
+    if (wordList.includes(word)) return;
+    // sets the updated text by adding the keywors to the end of sentence
+    // NOTE: I used this logic because the document provided says we cannot change the coherence of the original text
+    setUpdatedText(prev => prev.trim() + ' ' + word);
+  }
 
   return (
     <div className="App">
       <div className="navbar">
         <h1>SEO Analyzer Web Page</h1>
       </div>
-      {/* LEFT SIDE */}
       <div className='page'>
+        {/* LEFT SIDE */}
         <div className="left">
           <h3>Input Text:</h3>
+          {/* text aread where the input text will be entered */}
           <textarea
             rows="6"
             value={text}
             onChange={handleChange}
             placeholder="Enter your text here"
           />
+          {/* submit button */}
           <button onClick={handleSubmit}>Analyze</button>
-
+          {/* renders the keywords if they exist */}
           {Object.keys(keywords).length > 0 && (
             <div className="results">
               <h3>Keywords:</h3>
               <ul>
+                {/* keywords : count are shown here */}
+                {/* keywords are mapped and the variable i here is used to give them each a unique key */}
                 {Object.entries(keywords).map(([kw, count], i) => (
                   <li key={i}>
                     {kw} :{count}
+                    {/* insert button for each keyword */}
                     <button class="libtn" onClick={() => insertKeyword(kw)}>Insert</button>
                   </li>
                 ))}
@@ -69,7 +85,9 @@ function App() {
         {/* RIGHT SIDE */}
         <div className="right">
           <h3>Updated Text:</h3>
+          {/* textarea where the updated text will be shown */}
           <p className="text-preview">{updatedText}</p>
+          {/* copy button */}
           <button onClick={() => navigator.clipboard.writeText(updatedText)}>
             Copy Text
           </button>
